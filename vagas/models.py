10564c1c989cbb5897816c540f84389c11332a5d
Rodrigo Ricci <rodrigo@ricci.dev.br>
2022-11-11 20:57:43 -0300

    - Inclusão model Tarefas

--- vagas/models.py
+++ vagas/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from empresa.models import Vagas
+
+
+class Tarefa(models.Model):
+    choices_prioridade = (
+        ('B', 'Baixa'),
+        ('A', 'Alta'),
+        ('U', 'Urgente')
+    )
+    vaga = models.ForeignKey(Vagas, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=30)
+    prioridade = models.CharField(max_length=1, choices=choices_prioridade)
+    data = models.DateField()
+    realizada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
